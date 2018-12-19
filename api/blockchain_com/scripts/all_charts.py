16b3a2fda20a62baedda18cac430d91402517922
samz <samuel.zakalik@gmail.com>
2018-12-18 20:46:47 -0500

    Refactor `all_charts.py` for `blockchain_com`

--- api/blockchain_com/scripts/all_charts.py
+++ api/blockchain_com/scripts/all_charts.py
@@ -1,71 +1,59 @@
 # -*- coding: utf-8 -*
 """Gets all chart data from `blockchain.com` and loads it to S3.
-TODO: handle different data formats (csv --> DF --> parquet?)
 """
-import asyncio
 import time
+from multiprocessing import Pool
 
-import aiohttp
+import pandas as pd
+import requests as req
 
 from api.blockchain_com.helpers import get_chart_names_url_bases
-from helpers.aws import json_to_s3
+from helpers.aws import df_to_s3
 from helpers.general import get_utc_ts_str
 
 
-def get_chart_data(s3_folder_path_base='api/blockchain.com/charts/', timespan='all', data_format='json'):
-    """Get all data for all charts from `blockchain.com` API.
+def get_chart_data(chart_url_with_name):
+    """Gets `blockchain.com` chart data and uploads to S3.
 
     Args:
-        s3_folder_path_base (str): base folder path within S3 bucket for charts data.
-        timespan (str): chart timespan
-        data_format (str): (`json` or `csv`)
+        chart_url_with_name (tuple): (URL and corresponding name for given `blockchain.com` chart.
 
-    Returns:
+    Returns: Chart API data uploaded as Parquet file to S3.
 
     """
-    # get S3 file paths (+ chart names) and URLs to request chart data from `blockchain.com`
-    chart_names, url_bases = get_chart_names_url_bases()
-    url_suffix = "?timespan={0}&format={1}".format(timespan, data_format)
-    s3_file_paths = [s3_folder_path_base + cn + '/' for cn in chart_names]
-    chart_urls = [u + url_suffix for u in url_bases]
 
-    # asynchronously request chart data from `blockchain.com`
-    print("~~~ Requesting blockchain.com chart data ~~~")
-    start_time = time.time()
-    req_ts = get_utc_ts_str()
-    responses = []
-    resp_file_paths = []
-    resp_timestamps = []  # used as S3 file names
+    url = chart_url_with_name[0]
+    chart_name = chart_url_with_name[1]
+    request_ts = get_utc_ts_str()
 
-    async def get_charts_data(file_paths, urls):
-        async with aiohttp.ClientSession() as session:
-            for file_path, url in zip(file_paths, urls):
-                async with session.get(url) as resp:
-                    if resp.status == 200:
-                        responses.append(await resp.text())
-                        resp_file_paths.append(file_path)
-                        resp_timestamps.append(req_ts)
+    resp = req.get(url=url)
+    resp_json = resp.json()
+    df = pd.DataFrame(resp_json['values'])
 
-    # execute
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_charts_data(s3_file_paths, chart_urls))
+    df_to_s3(df=df,
+             target_bucket='data.crypto',
+             folder_path='api/blockchain.com/_' + chart_name,
+             file_name=request_ts,
+             print_message=False)
 
-    assert len(responses) == len(chart_names), "~~~ Data for {0} of {1} charts returned. ~~~".format(len(responses),
-                                                                                                     len(chart_names))
-    print("~~~ Successfully pulled all chart data in : %s minutes ~~~" % round((time.time() - start_time) / 60, 2))
-
-    return responses, resp_file_paths, resp_timestamps
+    return 0
 
 
 def main():
-    # get data
-    chart_responses, chart_file_paths, chart_resp_timestamps = get_chart_data()
-    # upload to S3
-    json_to_s3(response=chart_responses,
-               target_bucket='data.crypto',
-               file_path=chart_file_paths,
-               file_name=chart_resp_timestamps,
-               multiple=True)
+    timespan = 'all'
+    data_format = 'json'
+
+    # get S3 file paths (+ chart names) and URLs to request chart data from `blockchain.com`
+    chart_names, url_bases = get_chart_names_url_bases()
+    url_suffix = "?timespan={0}&format={1}".format(timespan, data_format)
+    chart_urls = [u + url_suffix for u in url_bases]
+    chart_urls_with_names = zip(chart_urls, chart_names)
+
+    print("~~~ Pulling `blockchain.com` chart data. ~~~")
+    start_time = time.time()
+    pool = Pool()
+    pool.map(get_chart_data, chart_urls_with_names)
+    print("~~~ All chart data pulled in: %s minutes ~~~" % round((time.time() - start_time) / 60, 2))
 
 
 if __name__ == '__main__':
